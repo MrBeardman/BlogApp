342846e3561b8c6e659b381a2b5e6eb000079217
John Matyas <22-7517-1@plus4u.net>
2022-05-26 21:59:52 +0200

    CRUD Implementatiot- Create user freature module, with user service and user controller, added postman requests and the useEntity and userInterface

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ConfigModule} from '@nestjs/config';
 import {TypeOrmModule} from '@nestjs/typeorm'; //for database
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -12,7 +13,7 @@ TypeOrmModule.forRoot({
   url : process.env.DATABASE_URL, //go to .env file and get the DATABASE_URL value
   autoLoadEntities : true,
   synchronize : true
-})
+}), UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
